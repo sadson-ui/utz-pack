0bb6269518b949732e2a5794b1a182be3a8873a5
sadson-ui <154842382+sadson-ui@users.noreply.github.com>
2023-12-30 17:01:04 +0100

    v1.0.2

--- [utz]-Pack/build - utzpack.bat
+++ [utz]-Pack/build - utzpack.bat
@@ -5,7 +5,7 @@
 del ".\utzpack.taco"
 
 :: uses WinRar to create a zip out of \Data and \YOUR XML
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\utzpack.zip" ".\Data" ".\_markerCategories.xml" ".\utz_Strikes.xml" ".\utz_1062_Wing1.xml" ".\utz_1149_Wing2.xml" ".\utz_1156_Wing3.xml" ".\utz_1188_Wing4.xml" ".\utz_1264_Wing5.xml" ".\utz_1303_Wing6.xml" ".\utz_1323_Wing7.xml" ".\utz_blkf.xml" ".\utz_missing.xml"*
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\utzpack.zip" ".\Data" ".\utz_AAA.xml" ".\utz_Strikes.xml" ".\utz_Raid_Wing1.xml" ".\utz_Raid_Wing2.xml" ".\utz_Raid_Wing3.xml" ".\utz_Raid_Wing4.xml" ".\utz_Raid_Wing5.xml" ".\utz_Raid_Wing6.xml" ".\utz_Raid_Wing7.xml" ".\utz_blkf.xml" ".\utz_Missing.xml"*
 
 :: renames the zip to taco
 rename "utzpack.zip" "utzpack.taco"
