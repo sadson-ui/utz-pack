38634d27d916eb61721b531e235150c4ddea6ba6
sadson <matthiasmemminger@gmx.de>
2024-12-02 23:26:33 +0100

    v1.0.7
    added wing 8

--- [utz]-Pack/build - utzpack.bat
+++ [utz]-Pack/build - utzpack.bat
@@ -1,5 +1,5 @@
 del ".\utzpack.taco"
 
-"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\utzpack.zip" ".\Data" ".\utz_AAA.xml" ".\utz_Strikes.xml" ".\utz_Raid_Wing1.xml" ".\utz_Raid_Wing2.xml" ".\utz_Raid_Wing3.xml" ".\utz_Raid_Wing4.xml" ".\utz_Raid_Wing5.xml" ".\utz_Raid_Wing6.xml" ".\utz_Raid_Wing7.xml" ".\utz_blkf.xml" ".\utz_Missing.xml"*
+"C:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 -ibck -r -y ".\utzpack.zip" ".\Data" ".\utz_AAA.xml" ".\utz_Strikes.xml" ".\utz_Raid_Wing1.xml" ".\utz_Raid_Wing2.xml" ".\utz_Raid_Wing3.xml" ".\utz_Raid_Wing4.xml" ".\utz_Raid_Wing5.xml" ".\utz_Raid_Wing6.xml" ".\utz_Raid_Wing7.xml" ".\utz_Raid_Wing8.xml" ".\utz_blkf.xml" ".\utz_Missing.xml" ".\utz_open_world.xml"*
 
 rename "utzpack.zip" "utzpack.taco"
